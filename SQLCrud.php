5c6cf47760bc0db17423b1ef882a73e858bd7424
rabiademirr <75272533+rabiademirr@users.noreply.github.com>
2021-02-18 22:53:32 +0300

    Update SQLCrud.php

--- SQLCrud.php
+++ SQLCrud.php
@@ -1,3 +1,40 @@
 <?php
+ include "SQLQuery.php";
+
+
+trait SQLCrud 
+{
+
+public function create (SQLQuery $query) {
+
+}
+public function update (SQLQuery $query) {
+
+}
+public function delete (SQLQuery $query) {
+
+}
+public function get (SQLQuery $query) {
+
+}
+public function first (SQLQuery $query) {
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+?>
+
 
 
