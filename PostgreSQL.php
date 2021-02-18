9c5ddfe5fad724d0da96304306e23f95a958fa60
rabiademirr <75272533+rabiademirr@users.noreply.github.com>
2021-02-18 22:52:40 +0300

    Update PostgreSQL.php

--- PostgreSQL.php
+++ PostgreSQL.php
@@ -1,3 +1,50 @@
 <?php
+include "SQL.php";
+include "SQLCrud.php";
+
+class PostgreSQL extends SQL
+{
+use SQLCrud;
+
+
+private $host="localhost";
+private $port=8080;
+private $username="root1";
+private $password=" ";
+private $database="postgresql";
+
+
+ public function __construct($host,$port,$username,$password,$database) 
+ {
+ 	$this->host=$host;
+ 	$this->port=$port;
+ 	$this->username=$username;
+ 	$this->password=$password;
+ 	$this->database=$database;
+
+ }
+ public function __destruct(){
+ 	//echo "this is destruct";
+ }
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
 
 
