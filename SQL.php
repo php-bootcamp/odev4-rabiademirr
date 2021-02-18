1e60bbd24879d5aba66f6b399069de3f0f62eece
rabiademirr <75272533+rabiademirr@users.noreply.github.com>
2021-02-18 22:53:07 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -1,3 +1,24 @@
 <?php
 
+include "SQLQuery.php";
+include "PDOConnector.php";
+
+abstract class SQL  {
+use PDOConnector;
+
+public  $connectionStatus=false;
+
+
+public abstract function isConnected();
+
+
+
+}
+
+
+
+
+
+?>
+
 
