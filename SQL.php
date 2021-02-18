7eac2eb09f4b29330db951f27c39d5ff7d67637f
rabiademirr <75272533+rabiademirr@users.noreply.github.com>
2021-02-18 22:59:56 +0300

    Update SQL.php

--- SQL.php
+++ SQL.php
@@ -8,6 +8,9 @@ use PDOConnector;
 
 public  $connectionStatus=false;
 
+public abstract function create(SQLQuery $query);
+public abstract function update(SQLQuery $query);
+public abstract function delete(SQLQuery $query);
 
 public abstract function isConnected();
 
