e4b73ca8334602c5ab2db2c7c3125b13127a1f80
rabiademirr <75272533+rabiademirr@users.noreply.github.com>
2021-02-18 22:53:56 +0300

    Update SQLQuery.php

--- SQLQuery.php
+++ SQLQuery.php
@@ -1,3 +1,67 @@
 <?php
+include "IQuery.php";
+include "SQL.php";
 
+
+class SQLQuery  extends SQL implements IQuery {
+
+
+
+private string  $table;
+private  array $bindings;
+private int $offset;
+private int $limit;
+private $wheres=array();
+private string $action;
+
+
+public function compile() {
+
+}
+public function setTable(string $table)
+{
+
+	return $this;
+}
+public function addBinding(string $key,string $value) 
+{
+return $this;
+}
+public function removeBinding(string $key) 
+{
+	return $this;
+}
+public function setPaginate(int $limit,int $offset) 
+{
+	return $this;
+}
+public function addWhere(string $column,string $operator, string $value) 
+{
+	return $this;
+}
+public function delete() 
+{
+	return $this;
+}
+public function update() 
+{
+	return $this;
+}
+public function select() 
+{
+	return $this;
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
 
